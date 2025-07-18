bf0f3c60abcc9f0c72f31644b51f53e868aa7795
George Lucas Martins <georgemartins804@gmail.com>
2025-07-18 13:12:30 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_coful-user
-add_lunch_combo omni_coful-userdebug
-add_lunch_combo omni_coful-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_coful-user \
+    twrp_coful-userdebug \
+    twrp_coful-eng
